1354b1cc5bc91bc32ad33231179c384e7d50b3ff
Peter919 <97379969+Peter919@users.noreply.github.com>
2023-01-02 14:14:31 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,24 @@
+#define main m
+        main[]={1827996710,1832188418,1827803726,1828519164,859334989,0},
+#define main ain
+        main=0;
+#define main(\
+        main)\
+        main##def
+        main(type)struct
+{int    main;}
+        main(
+        main);
+#undef  main
+        main(){
+        main\
+def     main;
+        main.
+        main=
+       'main'-
+        m[ain++];
+printf(&main.
+        main);
+if(     m[ain])
+{extern main();
+        main();}}
