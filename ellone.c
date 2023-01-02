132c10a4fab0eef9fcbcb15220ed66e428ab0bff
Peter919 <97379969+Peter919@users.noreply.github.com>
2023-01-02 14:19:37 +0100

    Update ellone.c
    Added return 0 at the end

--- ellone.c
+++ ellone.c
@@ -6,4 +6,4 @@
 #define l111 return
 #define l1ll for
 #define ll1l main
-ll1 ll1 l=1ll;ll1 ll1 l1l(ll ll1 ll1 l1l){ll1 ll1 l1=(1ll<<1ll)*(1ll+1ll);l111 l1*11l*(11*(l1+1l+1ll)+l1|1l)*((1l+1ll)*(l1-1l)*(l1+1ll)*(1l+l1)*(11+l1/(1l+1ll))*(11*(1l+l1+1l)*(11-1l)+1l)*(l1*1*(1l+11+1l)*(11*(l1-1l)-l1)*(l1*(1l+1l+l1)*(11*l1/(1l+1l)-l1+1l)+1l)+1l)-1ll)+l1l;}ll1 ll1 ll1l(){ll1 ll1 l1=1ll;{l11 ll1 ll1 l;l1+=l;}l+=l1;ll1 ll ll1*ll l=(ll1 ll1 ll*ll)("HeHe"+l1);lll1(l);{l11 ll1 ll1 l;l='l';l1ll(ll1 ll1 l1l=1;l1l<=l1;l1l+=1l){ll11(l);}}l1=l1l(11l);lll1(&l1);ll11(111-11l);ll11(11l*(1ll+1ll+1ll));}
+ll1 ll1 l=1ll;ll1 ll1 l1l(ll ll1 ll1 l1l){ll1 ll1 l1=(1ll<<1ll)*(1ll+1ll);l111 l1*11l*(11*(l1+1l+1ll)+l1|1l)*((1l+1ll)*(l1-1l)*(l1+1ll)*(1l+l1)*(11+l1/(1l+1ll))*(11*(1l+l1+1l)*(11-1l)+1l)*(l1*1*(1l+11+1l)*(11*(l1-1l)-l1)*(l1*(1l+1l+l1)*(11*l1/(1l+1l)-l1+1l)+1l)+1l)-1ll)+l1l;}ll1 ll1 ll1l(){ll1 ll1 l1=1ll;{l11 ll1 ll1 l;l1+=l;}l+=l1;ll1 ll ll1*ll l=(ll1 ll1 ll*ll)("HeHe"+l1);lll1(l);{l11 ll1 ll1 l;l='l';l1ll(ll1 ll1 l1l=1;l1l<=l1;l1l+=1l){ll11(l);}}l1=l1l(11l);lll1(&l1);ll11(111-11l);ll11(11l*(1ll+1ll+1ll));l111 11l&11;}
