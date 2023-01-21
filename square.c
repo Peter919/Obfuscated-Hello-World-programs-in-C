7c484777e840a471162e93e4245a507224a5ddf1
Peter919 <97379969+Peter919@users.noreply.github.com>
2023-01-21 14:14:59 +0100

    Create square.c

---
+++ square.c
@@ -0,0 +1,11 @@
+main(){char*c;putchar(
+('main'/'putchar')[c =
+main]);putchar(putchar
+(~-sizeof c+putchar( ~
+10+ "%" [c[0] &c [1<<1
+]]["putchar"])));int i
+[]= {033653440,999999+
+0x63E330}; printf (1 +
+i, i) ; puts (memcpy (
+alloca(' '), (int[]) {
+6581359 + 3}, 0x16));}
