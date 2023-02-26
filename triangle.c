fe86b1a312df2821bbdd5fb44feadc38ac123f59
Peter919 <97379969+Peter919@users.noreply.github.com>
2023-02-26 21:33:48 +0100

    Create triangle.c

---
+++ triangle.c
@@ -0,0 +1,19 @@
+;
+;f\
+oo()
+{ int
+* i  =
+strcpy(
+malloc(8
+),"=Cid!"
+); * i  +=
+0x032f32;p\
+uts(i);exit(
+0);} main() {
+putchar(' '-'\
+ '/'\n'+putchar
+(~1["??!!"]&'i')
+) ; int i [ ] = {
+745499756, 6497568
+};printf(i, 6511959
+) ; i [ 4 ] = foo; }
